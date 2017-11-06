0a868cf5ecbf0dc26f8c31b396ff18965a56becb
BrianPhiri <brianphiri.9523@gmail.com>
2017-11-06 23:02:36 +0300

    added logic for CountryEndpoint

--- soap_server/src/main/java/com/brianphiri/soap_server/CountryEndpoint.java
+++ soap_server/src/main/java/com/brianphiri/soap_server/CountryEndpoint.java
@@ -1,6 +1,26 @@
 package com.brianphiri.soap_server;
 
+import localhost._8080.webservice.GetCountryRequest;
+import localhost._8080.webservice.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
 public class CountryEndpoint {
     private static final String NAMESPACE_URL = "http://localhost:8080/webservice";
 
+    private CountryRepository countryRepository;
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository){
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URL, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountryResponse(@RequestPayload GetCountryRequest request){
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
+    }
 }
